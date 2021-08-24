97f107b102e7ab35a419f88e7ecdbadcec5736ce
jc782829 <joe.spelta@my.jcu.edu.au>
2021-08-24 12:56:09 +1000

    Add broken score program

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,3 +1,5 @@
+import random
+
 """
 CP1404/CP5632 - Practical
 Broken program to determine score status
@@ -15,15 +17,27 @@ Broken program to determine score status
 #     if score > 90:
 #     print("Excellent")
 # if score < 50:
-#     print("Bad"
+#     print("Bad")
 
 """Fixed Code"""
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-else:
-    print("Excellent")
+
+
+def main():
+    score = float(input("Enter score: "))
+    # score = random.randint(0, 100)
+    result = get_result(score)
+    print(score, result)
+
+
+def get_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+
+main()
