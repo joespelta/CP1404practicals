4231dbf35f7ba1bfb2f71d22d62a1dc9b6a05e9b
joespelta <joe.spelta@my.jcu.edu.au>
2021-09-05 19:58:31 +1000

    Add word occurrences program

---
+++ prac_05/word_occurences.py
@@ -0,0 +1,11 @@
+word_to_count = {}
+text = input("Text: ")
+words = text.split()
+for word in words:
+    word_frequency = word_to_count.get(word, 0)
+    word_to_count[word] = word_frequency + 1
+words = list(word_to_count.keys())
+words.sort()
+max_length = max((len(word) for word in words))
+for word in words:
+    print("{:{}} : {}".format(word, max_length, word_to_count[word]))
